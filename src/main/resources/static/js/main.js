5a20289e280f45081931da0852188c4d5f951857
Your Name <you@example.com>
2022-01-15 17:55:01 +0200

    Chat Join User

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,6 +2,8 @@
 var logInElement = document.querySelector('#login');
 var chatElement = document.querySelector('#chat');
 var userForm = document.querySelector('#userForm');
+var connect = document.querySelector('#connect');
+var mainChat = document.querySelector('#main-chat');
 var userName = null;
 var stomp = null;
 var URL = "http://localhost:8080"
@@ -17,12 +19,30 @@ function connectSocket(event){
     event.preventDefault()
 }
 function connectedDone(){
+    stomp.subscribe("/topic/all",sendMessage)
     stomp.send("/app/chat.logIn",{},
             JSON.stringify({sender: userName,chatType: 'JOIN'})
         )
-    stomp.subscribe("/app/chat.send",sendMessage)
-}
-function sendMessage(){
 
+    connect.classList.add('dis')
+}
+function sendMessage(payload){
+    var message = JSON.parse(payload.body)
+    if(message.chatType == 'JOIN'){
+        joinUser(message)
+    }
+}
+function joinUser(message){
+    var li1 = document.createElement('li');
+    var li2 = document.createElement('li');
+    var hr1 = document.createElement('hr');
+    var hr2 = document.createElement('hr');
+    var messageJoin = document.createTextNode(message.sender + ' join')
+    li1.classList.add('status');
+    li1.appendChild(messageJoin)
+    li2.appendChild(hr1)
+    li2.appendChild(li1)
+    li2.appendChild(hr2)
+    mainChat.appendChild(li2)
 }
 userForm.addEventListener('submit',connectSocket)
