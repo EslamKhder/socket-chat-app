0b56b2dc9a9779126b479d8ea11387b287630a06
Your Name <you@example.com>
2022-01-17 19:05:22 +0200

    front end active users

--- src/main/java/com/example/socketchatapp/controller/UserController.java
+++ src/main/java/com/example/socketchatapp/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.socketchatapp.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import com.example.socketchatapp.model.ActiveUser;
 import com.example.socketchatapp.model.Storage;
 
 @RestController
+@CrossOrigin("http://localhost:63342")
 public class UserController {
 
     @GetMapping("/active")
